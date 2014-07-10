6a1f23b011f46826a19325496b44abc1e98e48b4
Arne Schauf <git@as-webservices.de>
2014-07-10 18:48:22 +0200

    add task_id info view

--- django_saltstack/django_saltstack/settings/dev.py
+++ django_saltstack/django_saltstack/settings/dev.py
@@ -62,3 +62,8 @@ DEBUG_TOOLBAR_PATCH_SETTINGS = False
 # http://django-debug-toolbar.readthedocs.org/en/latest/installation.html
 INTERNAL_IPS = ('127.0.0.1',)
 ########## END TOOLBAR CONFIGURATION
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
--- django_saltstack/main/urls.py
+++ django_saltstack/main/urls.py
@@ -2,10 +2,11 @@
 
 from django.conf.urls import patterns, include, url
 
-from .views import TriggerSaltCommandView
+from .views import TriggerSaltCommandView, GetTaskInfoView
 
 
 urlpatterns = patterns(
     '',
     url(r'^$', TriggerSaltCommandView.as_view(), name='trigger_salt_command'),
+    url(r'^(?P<task_id>\d+)/$', GetTaskInfoView.as_view(), name='get_task_info'),
 )
