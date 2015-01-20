44e75db7a6ba1295310c033bf4be474d2ef80906
Arne Schauf <git@as-webservices.de>
2015-01-20 14:20:28 +0100

    run collectstatic

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd /opt/code/django_saltstack
+python manage.py collectstatic
 python manage.py syncdb --migrate
 exec gunicorn --workers 5 --log-level=debug -b 0.0.0.0:8000 django_saltstack.wsgi:application
