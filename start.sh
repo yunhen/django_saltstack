8d512e8fef99aaa59392a1aedec950bf4d48ed1b
Arne Schauf <git@as-webservices.de>
2015-01-20 15:42:13 +0100

    add --noinput to start.sh commands

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /opt/code/django_saltstack
-python manage.py collectstatic
-python manage.py syncdb --migrate
+python manage.py collectstatic --noinput
+python manage.py syncdb --migrate --noinput
 exec gunicorn --workers 5 --log-level=debug -b 0.0.0.0:8000 django_saltstack.wsgi:application
